10019873fab66afea07cc62f8f45264831e80cc9
Sachayanthan <sachayanthan.v.1999@gmail.com>
2023-01-25 20:39:36 +0530

    New post DOM [prepend] for Adding a post

---
+++ Projects/codialTemplate/assets/JS/home_posts.js
@@ -0,0 +1,58 @@
+{
+    // submit button handler through AJAX
+    let createPost = function() {
+        let newPostForm = $('#new-post-form');
+
+        console.log(newPostForm);
+
+        newPostForm.submit(function(event) {
+            event.preventDefault();
+            
+            $.ajax({
+                type : 'post',
+                url : '/posts/create',
+                data : newPostForm.serialize(),
+                success : function(data) {
+                    let newPost = newPostDom(data.data.post);
+                    $('#innerMain>ul').prepend(newPost);
+                },
+                error : function(err) {
+                    console.log(`Error :: ${err.responseText}`);
+                }
+            })
+
+        });
+    }
+
+    // method to create a post in DOM 
+    let newPostDom = function(post) {
+        console.log("This is the post ::: ", post);
+        return $(`<li id="post-${post._id} " class="postLi"> 
+                    <div class="postheader">
+                        <div class="userDetails">
+                            <h1> ${post.user.name}  </h1>
+                        </div>
+                        <a class="DeletePost delete-post-btn" href="/posts/destroy/${post.id}">Delete</a>
+                    </div>
+                    
+                    <div class="postContent">
+                        <p> ${ post.content } </p>
+                    </div>             
+                
+                    <div class="post-comments">
+                        <form action="/comments/create" method="POST">
+                            <input type="text" name="content" placeholder="Type here to add comment..." required>
+                            <input type="hidden" name="post" value="${post._id}">
+                            <input type="submit" value="Add Comment">
+                        </form>
+                        <div class="post-comments-list">
+                            <ul class="commentUl" id="post-comments-${ post._id }">
+                            </ul>
+                        </div>
+                    </div>
+                </li> `
+    )
+    }
+
+    createPost();
+}
--- Projects/codialTemplate/controllers/posts_controller.js
+++ Projects/codialTemplate/controllers/posts_controller.js
@@ -4,11 +4,28 @@ const Comment = require('./../models/comment');
 module.exports.create = async function(req, res){
     
     try {
-        await Post.create({
+        let post = await Post.create({
             content : req.body.content,
             user : req.user._id 
         });
+
+        console.log(post);
+
+        if(req.xhr) {
+            return res.json(200, {
+                data : {
+                    post : post
+                },
+                message : "Post Created!"
+            });
+        }
+        else {
+            console.log('no post xhr');
+        }
+
+        req.flash('success', 'Post published');
     }catch(err) {
+        req.flash('error', err);
         console.log('Error Occured in post Controller CREATE!!!', err);
     }
 
